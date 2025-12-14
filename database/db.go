4c0cb8334ec822d23d68b3157b9acaffe782e84f
satyav <satyamsc@gmail.com>
2025-12-15 00:07:45 +0100

    Recreate database/db.go; rebuild binary

--- database/db.go
+++ database/db.go
@@ -1,19 +1,18 @@
 package database
 
 import (
-	"go-backend/internal/models"
-
-	"github.com/glebarez/sqlite"
-	"gorm.io/gorm"
+    "go-backend/internal/models"
+    "github.com/glebarez/sqlite"
+    "gorm.io/gorm"
 )
 
 func Connect(path string) (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-	if err := db.AutoMigrate(&models.Device{}); err != nil {
-		return nil, err
-	}
-	return db, nil
+    db, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
+    if err != nil {
+        return nil, err
+    }
+    if err := db.AutoMigrate(&models.Device{}); err != nil {
+        return nil, err
+    }
+    return db, nil
 }
